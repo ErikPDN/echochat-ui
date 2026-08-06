3da944f3749fc3a21b5b31c8db018c1b2bcf35c4
Erik Neves <erikpdn1@gmail.com>
2026-08-06 20:53:43 -0300

    feat: adiciona toast global

--- app/providers/Providers.tsx
+++ app/providers/Providers.tsx
@@ -3,13 +3,29 @@
 import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./AuthProvider";
+import { toast, Toaster } from "sonner";
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>{children}</AuthProvider>
+      <AuthProvider>
+        {children}
+        <Toaster
+          theme="dark"
+          position="top-right"
+          toastOptions={{
+            classNames: {
+              toast: "bg-card! ",
+              title: "text-sm! font-medium!",
+              error: "border-2! border-red-500/30! [&>div>svg]:text-red-400!",
+              success:
+                "border-2! border-green-500/30! [&>div>svg]:text-green-400!",
+            },
+          }}
+        />
+      </AuthProvider>
     </QueryClientProvider>
   );
 };
